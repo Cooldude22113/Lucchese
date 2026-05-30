e90f54a8fedf8abdd6e120b2b79955776c0c59ad
Cooldude22113 <ashammond21@gmail.com>
2026-05-30 07:44:15 +0100

    untrack archive

--- Archive/wipe_chroma.py
+++
@@ -1,9 +0,0 @@
-import chromadb
-client = chromadb.PersistentClient(path='./chroma_db')
-for name in ['knowledge', 'facts', 'style', 'documents', 'summaries']:
-    try:
-        client.delete_collection(name)
-        print(f'Deleted {name}')
-    except Exception as e:
-        print(f'{name} not found: {e}')
-print('Done')
